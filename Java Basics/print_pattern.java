de11fde70c448c52f64b6db86527c41160ba6fe5
Sunny Kumar Pandey <sunnypandey9708733261@gmail.com>
2025-06-09 23:09:01 +0530

    java

---
+++ Java Basics/print_pattern.java
@@ -0,0 +1,10 @@
+
+public class print_pattern {
+    public static void main(String[] args) {
+        System.out.println("* * * *");
+        System.err.println("* * *");
+        System.err.println("* *");
+        System.err.println("*");
+    }
+    
+}
