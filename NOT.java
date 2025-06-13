b530c70ed37bc4db2d25e428bd211a8000e97865
Shubham kumar <shubhamlalita07@gmail.com>
2025-06-13 17:21:20 +0530

    Add files via upload

---
+++ NOT.java
@@ -0,0 +1,9 @@
+
+
+public class NOT {
+    public static void main(String[] args) {
+        System.out.println(!(0>5));
+        System.out.println(!(5>2));
+    }
+    
+}
---
+++ OR.java
@@ -0,0 +1,8 @@
+
+
+public class OR {
+    public static void main(String[] args) {
+        System.out.println((3>4) || (4<1));
+        System.out.println((3<2) || (7>5));
+    }
+}
