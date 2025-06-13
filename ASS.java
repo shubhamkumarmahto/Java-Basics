493152f9d0afc79444357d64855278293aecf3f8
Shubham kumar <shubhamlalita07@gmail.com>
2025-06-13 17:20:40 +0530

    Add files via upload

---
+++ ASS.java
@@ -0,0 +1,21 @@
+
+
+public class ASS {
+    public static void main(String[] args) {
+        int A = 10;
+        A += 10;
+        int B = 10;
+        B -= 10;
+        int C = 10;
+        C *= 10;
+        int D = 10;
+        D /= 10;
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(C);
+        System.out.println(D);
+
+
+    }
+    
+}
