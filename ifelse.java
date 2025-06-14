835f251c903169a8a028b00837110acfef795ac1
Shubham kumar <shubhamlalita07@gmail.com>
2025-06-14 13:15:45 +0530

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,17 @@
+
+
+public class ifelse {
+    public static void main(String[] args) {
+        int age = 16;
+        if(age >= 18){
+            System.out.println("adult : vote, drive");
+        }
+        if(age >= 13 && age <=18){
+            System.out.println("teengaer");
+        }
+        else{
+            System.out.println("not adult");
+        }
+    }
+    
+}
---
+++ practice.java
@@ -0,0 +1,15 @@
+
+
+public class practice {
+    public static void main(String[] args) {
+        int A = 1;
+        int B = 3;
+        if(A >= B){
+            System.out.println("A");
+        }
+        else{
+            System.out.println("B");
+        }
+    }
+    
+}
