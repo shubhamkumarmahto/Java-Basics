af950b27e91d37dbac82057428ea0fb48d11d2bf
Shubham kumar <shubhamlalita07@gmail.com>
2025-06-16 21:58:41 +0530

    Add files via upload

---
+++ ind.java
@@ -0,0 +1,9 @@
+public class ind {
+    public static void main(String[] args) {
+        int A = 20;
+        int B = 22;
+        int sum = A + B;
+        System.out.println("add of two :" + sum);
+    }
+    
+}
