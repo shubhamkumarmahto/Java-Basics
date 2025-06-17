5f88849edee292fd652deb65df82f915aaad1c18
Shubham kumar <shubhamlalita07@gmail.com>
2025-06-17 21:49:07 +0530

    Add files via upload

---
+++ witch.java
@@ -0,0 +1,15 @@
+public class witch{
+    public static void main(String[] args) {
+        int number = 2;
+        switch(number){
+            case 1 : System.out.println("bread");
+                       break;
+            case 2 : System.out.println("samosha");
+                        break;
+            case 3 : System.out.println("kella");
+                     break;
+            default : System.out.println("KUch nhi mila");
+        }
+    }
+    
+}
