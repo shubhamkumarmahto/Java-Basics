efc6ae08ff8418ca0431309c47b14349aa345702
Shubham kumar <shubhamlalita07@gmail.com>
2025-06-20 13:00:36 +0530

    Add files via upload

---
+++ done.java
@@ -0,0 +1,10 @@
+public class done {
+    public static void main(String[] args) {
+        int counter = 1;
+        do { 
+            System.out.println("Hello world");
+            counter++;
+        } while (counter <= 10);
+    }
+    
+}
