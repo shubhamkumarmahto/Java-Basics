dd85e896b250f4356aa8d90bd6e8920954c8e66e
Shubham kumar <shubhamlalita07@gmail.com>
2025-06-20 12:59:16 +0530

    Add files via upload

---
+++ breaak.java
@@ -0,0 +1,12 @@
+public class breaak {
+    public static void main(String[] args) {
+        for(int i =1; i <= 5; i++){
+            if(i==3){
+                break;
+            }
+          System.out.println(i);
+        }
+        System.out.print("I am out of the loop");
+    }
+    
+}
