d1bace614a6c572686cbabd25e75d290b00ccdc4
Shubham kumar <shubhamlalita07@gmail.com>
2025-06-23 20:43:12 +0530

    Add files via upload

---
+++ func.java
@@ -0,0 +1,12 @@
+public class func {
+    public static void printHelloworld(){
+        System.out.println("hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+    }
+    public static void main(String[] args) {
+        printHelloworld();
+        
+    }
+    
+}
