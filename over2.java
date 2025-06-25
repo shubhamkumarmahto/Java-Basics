5a062fa62cb48528a612f4acb83d1b8997ae6c87
Shubham kumar <shubhamlalita07@gmail.com>
2025-06-25 11:46:33 +0530

    Add files via upload

---
+++ over2.java
@@ -0,0 +1,14 @@
+public class over2 {
+    public static int sum(int a, int b){
+        return a+b;
+    }
+    public static float sum(float c,float d){
+        return c+d;
+    } 
+    public static void main(String[] args) {
+        System.out.println(sum(4,5));
+        System.out.println(sum(0.4f,0.5f));
+        
+    }
+    
+}
